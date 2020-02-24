d9bf064f838dd91e71ba6a36882ec93783b818e1
Sabrine Mokhtar <sabrine_mokhtar@yahoo.fr>
2020-02-24 14:10:39 +0100

    Creation classe Fournisseur

---
+++ BT.Stage.SGIMI.Data.Entity/Fournisseur.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT.Stage.SGIMI.Data.Entity
+{
+    public class Fournisseur
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public int CreatedBy { get; set; }
+
+    }
+}
