1a05a14e7292b54fa721ec856a65f5f2c1e2f497
mohsninourelhouda <mohsninourelhouda98@gmail.com>
2020-02-25 12:54:23 +0100

    Create Materiel entity

---
+++ BT.Stage.SGIMI.Data.Entity/Materiel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT.Stage.SGIMI.Data.Entity
+{
+    class Materiel
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Reference { get; set; }
+        //doit etre changer par un attribut significatif
+        public int Fournisseur { get; set; }
+        public int CreatedBy { get; set; }
+    }
+}
