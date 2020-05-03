ecb9165f5458fe1b0799f55870c88da0290686e2
Sabrine Mokhtar <sabrine_mokhtar@yahoo.fr>
2020-05-03 19:50:26 +0100

    Input Control for DATA/Intervention

--- BT.Stage.SGIMI.Data.Entity/Intervention.cs
+++ BT.Stage.SGIMI.Data.Entity/Intervention.cs
@@ -3,26 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace BT.Stage.SGIMI.Data.Entity
 {
     public class Intervention
     {
         public int Id { get; set; }
+        [Required]
         public string Type { get; set; }
+
+
         public string Nature { get; set; }
+        [Display(Name = "Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public string Date { get; set; }
+
+
         public string Etat { get; set; }
+
+
         public int Reclamation { get; set; }
+
+
+        [RegularExpression(@"^[A - Z] +[a - zA - Z'\s]*$")]
+        [Required(ErrorMessage = "Veuillez exprimer le problème constaté")]
+        [MaxLength(100)]
+        [Display(Name = " Problème Constaté")]
         public string ProblèmeConstaté { get; set; }
+
+
+
+        [RegularExpression(@"^[A - Z] +[a - zA - Z'\s]*$")]
+        [Required(ErrorMessage = "Veuillez exprimer les traveaux effectués")]
+        [MaxLength(100)]
+        [Display(Name = " Les traveaux effectués")]
         public string TraveauxEffectués { get; set; }
+
+
         public string CreatedBy { get; set; }
         public string CreatedDate { get; set; }
         public string CreatedTime { get; set; }
-        public string LastUpdatedBy { get; set; }
         public string LastUpdatedDate { get; set; }
         public string LastUpdatedTime { get; set; }
-
-
+        public string LastUpdatedBy { get; set; }
     }
 }
