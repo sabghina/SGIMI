5008fd4f264fffc46a4ccc3132ea2e0ae12772ef
Sabrine Mokhtar <sabrine_mokhtar@yahoo.fr>
2020-05-03 19:55:54 +0100

    Control Input for DATA/Reclamation

--- BT.Stage.SGIMI.Data.Entity/Reclamation.cs
+++ BT.Stage.SGIMI.Data.Entity/Reclamation.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace BT.Stage.SGIMI.Data.Entity
 {
     public class Reclamation
@@ -11,9 +11,20 @@ namespace BT.Stage.SGIMI.Data.Entity
         public int Id { get; set; }
         public string Materiel { get; set; }
         public string Date { get; set; }
+
+        [RegularExpression(@"^[A - Z] +[a - zA - Z'\s]*$")]
+        [Required(ErrorMessage = "Veuillez exprimer votre problème")]
+        [Display(Name = "Problème à réclamer")]
         public string Probleme { get; set; }
+
+
+        [RegularExpression(@"^[A - Z] +[a - zA - Z'\s]*$")]
+        [Required(ErrorMessage = "Veuillez saisir un commentaire")]
+        [Display(Name = "Commentaire")]
         public string Commentaire { get; set; }
         public string Etat { get; set; }
         public int CreatedBy { get; set; }
     }
 }
+
+
