86be485ac606bbc0f80aa4108b8535c4afb29ff9
Sabrine Mokhtar <sabrine_mokhtar@yahoo.fr>
2020-05-03 03:28:17 +0100

    input control on InterventionViewModel

--- BT.Stage.SGIMI.UserInterface.ViewModel/InterventionViewModel.cs
+++ BT.Stage.SGIMI.UserInterface.ViewModel/InterventionViewModel.cs
@@ -12,12 +12,36 @@ namespace BT.Stage.SGIMI.UserInterface.ViewModel
         public int Id { get; set; }
         [Required]
         public string Type { get; set; }
+
+
         public string Nature { get; set; }
+        [Display(Name = "Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public string Date { get; set; }
+
+
         public string Etat { get; set; }
+
+    
         public int Reclamation { get; set; }
+        
+
+        [RegularExpression(@"^[A - Z] +[a - zA - Z'\s]*$")]
+        [Required(ErrorMessage = "Veuillez exprimer le problème constaté")]
+        [MaxLength(100)]
+        [Display(Name = " Problème Constaté")]
         public string ProblèmeConstaté { get; set; }
+
+
+
+        [RegularExpression(@"^[A - Z] +[a - zA - Z'\s]*$")]
+        [Required(ErrorMessage = "Veuillez exprimer les traveaux effectués")]
+        [MaxLength(100)]
+        [Display(Name = " Les traveaux effectués")]
         public string TraveauxEffectués { get; set; }
+
+
         public string CreatedBy { get; set; }
         public string CreatedDate { get; set; }
         public string CreatedTime { get; set; }
