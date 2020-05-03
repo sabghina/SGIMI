cd913690418f42e6dab0b3a6e122b1dc0a961ea6
Sabrine Mokhtar <sabrine_mokhtar@yahoo.fr>
2020-05-03 19:40:23 +0100

    Input Control on ReclamationViewModel

--- BT.Stage.SGIMI.UserInterface.ViewModel/ReclamationViewModel.cs
+++ BT.Stage.SGIMI.UserInterface.ViewModel/ReclamationViewModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace BT.Stage.SGIMI.UserInterface.ViewModel
 {
@@ -11,7 +13,16 @@ namespace BT.Stage.SGIMI.UserInterface.ViewModel
         public int Id { get; set; }
         public string Materiel { get; set; }
         public string Date { get; set; }
+
+        [RegularExpression(@"^[A - Z] +[a - zA - Z'\s]*$")]
+        [Required(ErrorMessage = "Veuillez exprimer votre problème")]
+        [Display(Name = "Problème à réclamer")]
         public string Probleme { get; set; }
+
+
+        [RegularExpression(@"^[A - Z] +[a - zA - Z'\s]*$")]
+        [Required(ErrorMessage = "Veuillez saisir un commentaire")]
+        [Display(Name = "Commentaire")]
         public string Commentaire { get; set; }
         public string Etat { get; set; }
         public int CreatedBy { get; set; }
