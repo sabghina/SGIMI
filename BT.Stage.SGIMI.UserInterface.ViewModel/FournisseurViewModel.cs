9a6f80edb93cc3d552e210d0defe52d6e0509c29
Sabrine Mokhtar <sabrine_mokhtar@yahoo.fr>
2020-05-03 03:11:05 +0100

    Input control on FournisseurViewModel

--- BT.Stage.SGIMI.UserInterface.ViewModel/FournisseurViewModel.cs
+++ BT.Stage.SGIMI.UserInterface.ViewModel/FournisseurViewModel.cs
@@ -11,19 +11,44 @@ namespace BT.Stage.SGIMI.UserInterface.ViewModel
     public class FournisseurViewModel
     {
         public int Id { get; set; }
-        [Required]
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$")]
+        [Required(ErrorMessage = "Veuillez saisir le nom du fournisseur")]
         [MaxLength(30)]
+        [Display(Name = " Nom")]
         public string Nom { get; set; }
+
+        
+        [Required(ErrorMessage = "Veuillez saisir le mail")]
         [MaxLength(30)]
+        [Display(Name = " Email")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [RegularExpression(@"(\+00216[0-9]{2}|0)([0-9]{9}|[0-9\-\s]{9,18})")]
+        [StringLength(15, MinimumLength = 8, ErrorMessage = "Longueur non valide")]
+        [Required(ErrorMessage = "Veuillez saisir le numéro de téléphone du fournisseur")]
         [MaxLength(15)]
+        [Display(Name = " Téléphone")]
         public string Telephone { get; set; }
+
+        
+        [Required(ErrorMessage = "Veuillez saisir l'adresse du fournisseur")]
         [MaxLength(30)]
+        [Display(Name = " Adresse")]
         public string Adresse { get; set; }
+
+        [RegularExpression(@"(\+00216[0-9]{2}|0)([0-9]{9}|[0-9\-\s]{9,18})")]
+        [Required(ErrorMessage = "Veuillez saisir le fax du fournisseur")]
         [MaxLength(15)]
+        [Display(Name = " Fax")]
         public string Fax { get; set; }
+
+        [Required(ErrorMessage = "Veuillez saisir le site web du fournisseur")]
         [MaxLength(30)]
+        [Display(Name = "Site Web")]
         public string SiteWeb { get; set; }
+
         /// <summary>
         /// F: Fourniseur, S: SociétéTierce
         /// </summary>
