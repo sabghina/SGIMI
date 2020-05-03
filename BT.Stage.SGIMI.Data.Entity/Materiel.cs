efcb02bb673cadd9d96fb4d00f826bd2b6969b52
Sabrine Mokhtar <sabrine_mokhtar@yahoo.fr>
2020-05-03 19:53:50 +0100

    Input Control for DATA/Materiel

--- BT.Stage.SGIMI.Data.Entity/Materiel.cs
+++ BT.Stage.SGIMI.Data.Entity/Materiel.cs
@@ -3,17 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace BT.Stage.SGIMI.Data.Entity
 {
     public class Materiel
     {
+          
+
         public int Id { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$")]
+        [Required(ErrorMessage = "Veuillez saisir le nom du matériel")]
+        [MaxLength(20)]
+        [Display(Name = " Nom")]
         public string Nom { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$")]
+        [Required(ErrorMessage = "Veuillez saisir la marque du matériel")]
+        [MaxLength(10)]
+        [Display(Name = " Marque")]
         public string Marque { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$")]
+        [Required(ErrorMessage = "Veuillez saisir le modèle du matériel")]
+        [MaxLength(10)]
+        [Display(Name = " Modèle")]
         public string Modele { get; set; }
+
+
+        [Required(ErrorMessage = "Veuillez saisir la référence du matériel")]
+        [MaxLength(20)]
+        [Display(Name = " Réference BT")]
         public string ReferenceBT { get; set; }
+
+        [RegularExpression(@"[0-9]{2}|0)([0-9]{9}|[0-9\-\s]{9,18})")]
+        [Required(ErrorMessage = "Veuillez saisir le nom du fournisseur")]
+        [MaxLength(30)]
+        [Display(Name = " Num de Serie")]
         public int NumeroDeSerie { get; set; }
+
         public int Fournisseur { get; set; }
         public string CreatedDate { get; set; }
         public string CreatedTime { get; set; }
