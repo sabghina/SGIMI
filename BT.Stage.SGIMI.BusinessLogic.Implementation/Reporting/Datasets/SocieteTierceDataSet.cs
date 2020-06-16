e727d273c8f9718678bb98ffc111a3b017ca01f4
Mokhtar Sabrine <sabrine_mokhtar@yahoo.fr>
2020-06-16 06:12:26 +0200

    Fixed DynamicReport Societe Tierce

---
+++ BT.Stage.SGIMI.BusinessLogic.Implementation/Reporting/Datasets/SocieteTierceDataSet.cs
@@ -0,0 +1,25 @@
+﻿namespace BT.Stage.SGIMI.BusinessLogic.Implementation.Reporting.Datasets
+{
+}
+
+namespace BT.Stage.SGIMI.BusinessLogic.Implementation.Reporting.Datasets
+{
+}
+
+namespace BT.Stage.SGIMI.BusinessLogic.Implementation.Reporting.Datasets
+{
+}
+namespace BT.Stage.SGIMI.BusinessLogic.Implementation.Reporting.Datasets
+{
+
+
+    public partial class FournisseurDataSet
+    {
+    }
+}
+namespace BT.Stage.SGIMI.BusinessLogic.Implementation.Reporting.Datasets {
+    
+    
+    public partial class FournisseurDataSet {
+    }
+}
