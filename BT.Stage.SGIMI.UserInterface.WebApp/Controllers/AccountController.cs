ad048d023fc3aa184eb31c1442e4f3f931710e62
Mokhtar Sabrine <sabrine_mokhtar@yahoo.fr>
2020-06-25 21:49:22 +0200

    Implemented User Charts

--- BT.Stage.SGIMI.UserInterface.WebApp/Controllers/AccountController.cs
+++ BT.Stage.SGIMI.UserInterface.WebApp/Controllers/AccountController.cs
@@ -69,14 +69,22 @@ namespace BT.Stage.SGIMI.UserInterface.WebApp.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
         {
+            
+            // This doesn't count login failures towards account lockout
+            // To enable password failures to trigger account lockout, change to shouldLockout: true
+            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
             if (!ModelState.IsValid)
             {
                 return View(model);
             }
-
-            // This doesn't count login failures towards account lockout
-            // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            if (User.IsInRole("admin"))
+            {
+                returnUrl = "/";
+            }
+            else if (User.IsInRole("user"))
+            {
+                returnUrl = "/ChartReclamation";
+            }
             switch (result)
             {
                 case SignInStatus.Success:
--- BT.Stage.SGIMI.UserInterface.WebApp/Controllers/ChartReclamationController.cs
+++ BT.Stage.SGIMI.UserInterface.WebApp/Controllers/ChartReclamationController.cs
@@ -10,30 +10,65 @@ using System.Web.Mvc;
 
 namespace BT.Stage.SGIMI.UserInterface.WebApp.Controllers
 {
-[Authorize]
+    [Authorize]
     public class ChartReclamationController : Controller
     {
         readonly IReclamationRepository reclamationRepository;
-        public ChartReclamationController(IReclamationRepository _reclamationRepository)
+        readonly IInterventionRepository interventionRepository;
+        readonly IMaterielRepository materielRepository;
+        public ChartReclamationController(IReclamationRepository _reclamationRepository,
+            IMaterielRepository _materielRepository, IInterventionRepository _interventionRepository)
         {
             reclamationRepository = _reclamationRepository;
+            materielRepository = _materielRepository;
+            interventionRepository = _interventionRepository;
         }
+
+
         // GET: ChartReclamation
         public ActionResult Index()
         {
-            string currentUser = User.Identity.Name;
-            List<Reclamation> reclamations = reclamationRepository.GetUserReclamations(currentUser);
-            var nbReclamationByMateriel = reclamations.GroupBy(Reclamation => Reclamation.Materiel)
-                .Select(groupedReclamation => new { materiel = groupedReclamation.Key, materiels = groupedReclamation.ToList() })
+            //Liste des reclamations
+            List<Reclamation> reclamationsOnHold = reclamationRepository.GetReclamations();
+            List<Reclamation> reclamationsInProgress = reclamationRepository.GetInProgressReclamations();
+            List<Reclamation> reclamationsCanceled = reclamationRepository.GetCanceledReclamations();
+            List<Reclamation> reclamationsFinished = reclamationRepository.GetFinishedReclamations();
+            List<Reclamation> reclamations = reclamationsOnHold;
+            foreach (Reclamation reclamation in reclamationsInProgress)
+            {
+                reclamations.Add(reclamation);
+            }
+            foreach (Reclamation reclamation in reclamationsCanceled)
+            {
+                reclamations.Add(reclamation);
+            }
+            foreach (Reclamation reclamation in reclamationsFinished)
+            {
+                reclamations.Add(reclamation);
+            }
+            //Nombre des reclamations par etat
+            var nbReclamationByEtat = reclamations.GroupBy(Reclamation => Reclamation.Etat)
+            .Select(groupedReclamation => new { etat = groupedReclamation.Key, etats = groupedReclamation.ToList() }).ToList();
+            //Nombre des reclamations par date
+            var nbReclamationByDate = reclamations.GroupBy(Reclamation => Reclamation.CreatedDate)
+                .Select(groupedReclamation => new { date = groupedReclamation.Key, dates = groupedReclamation.ToList() })
                 .ToList();
 
+            // Liste des reclamations par date
             List<DataPoint> dataPoints = new List<DataPoint>();
-            foreach (var groupReclamation in nbReclamationByMateriel)
+            foreach (var groupReclamation in nbReclamationByDate)
             {
-                //dataPoints.Add(new DataPoint(groupReclamation.materiel, groupReclamation.materiels.Count()));
+                dataPoints.Add(new DataPoint(groupReclamation.date, groupReclamation.dates.Count()));
+            }
+            //Liste des reclamations par etats
+            List<DataPoint> dataPoints2 = new List<DataPoint>();
+            foreach (var groupReclamation in nbReclamationByEtat)
+            {
+                dataPoints2.Add(new DataPoint(groupReclamation.etat, groupReclamation.etats.Count()));
             }
 
-            ViewBag.NombreReclamationPar = JsonConvert.SerializeObject(dataPoints);
+            ViewBag.NombreReclamationParDate = JsonConvert.SerializeObject(dataPoints);
+            ViewBag.NombreReclamationParEtat = JsonConvert.SerializeObject(dataPoints2);
             return View();
         }
     }
