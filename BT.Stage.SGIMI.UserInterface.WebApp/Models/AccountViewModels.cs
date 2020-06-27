51b4775e60d8cdf4b4a1f59820770a486ec4d29f
Mokhtar Sabrine <sabrine_mokhtar@yahoo.fr>
2020-06-27 16:17:47 +0200

    Desgined all the views everywhere

--- BT.Stage.SGIMI.UserInterface.WebApp/Models/AccountViewModels.cs
+++ BT.Stage.SGIMI.UserInterface.WebApp/Models/AccountViewModels.cs
@@ -72,11 +72,11 @@ namespace BT.Stage.SGIMI.UserInterface.WebApp.Models
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Mot de passe")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "Confirmer mot de passe")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
